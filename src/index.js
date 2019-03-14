2a8c7781ab82b8a052d56d7142c324d42b485247
brian <briancjkim91@gmail.com>
2019-03-14 18:05:00 +1000

    change index.js startOptions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./resolvers2";
+
 const PORT = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT = "/playground";
+const GRAPHQL_ENDPOINT = "/graphql";
+
+const serverOptions = {
+  port: PORT,
+  palyground: PLAYGROUND_ENDPOINT,
+  endPoint: GRAPHQL_ENDPOINT
+};
 const server = new GraphQLServer({
   port: PORT,
   typeDefs: "src/schema.graphql",
   resolvers
 });
 
-server.start(() => console.log(`graphql server Running on port ${PORT}`));
+server.start(serverOptions, ({ port }) =>
+  console.log(`graphql server Running on port ${port}`)
+);
