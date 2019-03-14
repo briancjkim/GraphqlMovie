50002559f1da4dfe8e4819e327bd5f61017a2e94
brian <briancjkim91@gmail.com>
2019-03-14 16:39:48 +1000

    -test heroku

---
+++ graphql/resolvers2.js
@@ -0,0 +1,11 @@
+import { getMovie, getMovies } from './db2';
+import { getSuggestions } from './db';
+const resolvers = {
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
+}
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { GraphQLServer } from "graphql-yoga";
-import resolvers from "./graphql/resolvers";
-
+import resolvers from "./graphql/resolvers2";
+const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({
+  port: PORT,
   typeDefs: "graphql/schema.graphql",
   resolvers
 });
 
-server.start(() => console.log("graphql server Running"));
+server.start(() => console.log(`graphql server Running on port ${PORT}`));
